d64714be2d2e1368e801825ab4c929207cef9824
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-26 20:00:14 +0100

    added annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator) -> str :
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str :
     """
     filter Datum function
     Args:
